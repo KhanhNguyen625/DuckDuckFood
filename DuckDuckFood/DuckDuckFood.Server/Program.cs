282cada5e7067ab6cb1ca4236931b98f35fb3fcd
Calvin Phung <RynsArgent@hotmail.com>
2023-12-04 23:44:08 -0800

    CP Setup Startup class

--- DuckDuckFood/DuckDuckFood.Server/Program.cs
+++ DuckDuckFood/DuckDuckFood.Server/Program.cs
@@ -6,33 +6,12 @@ namespace DuckDuckFood.Server
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            Startup startup = new Startup(builder.Configuration);
+            startup.ConfigureServices(builder.Services);
 
             WebApplication app = builder.Build();
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.MapFallbackToFile("/index.html");
+            startup.Configure(app, app.Environment);
 
             app.Run();
         }
---
+++ DuckDuckFood/DuckDuckFood.Server/Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace DuckDuckFood.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // Configure the HTTP request pipeline.
+            if (environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Routing is implicitly included but added for completeness.
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+
+        }
+    }
+}
